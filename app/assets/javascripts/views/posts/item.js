aacb6ebaa22d5e563947a4edf5010a44aeba1d48
Zachary M Reisman <zach.reisman@gmail.com>
2015-06-14 20:12:03 -0700

    Convert PostItem view to CompositeView

--- app/assets/javascripts/views/posts/item.js
+++ app/assets/javascripts/views/posts/item.js
@@ -1,5 +1,6 @@
-Hastigram.Views.PostItem = Backbone.View.extend({
+Hastigram.Views.PostItem = Backbone.CompositeView.extend({
   template: JST['posts/item'],
+  className: "post maxwidth",
 
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
@@ -7,6 +8,8 @@ Hastigram.Views.PostItem = Backbone.View.extend({
 
   render: function() {
     this.$el.html( this.template({ post: this.model }) );
+    this.attachSubviews();
+
     return this;
   }
 
