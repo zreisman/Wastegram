e65206da2a7cc8b4e300c308a04765e408c26b12
Zachary M Reisman <zach.reisman@gmail.com>
2015-06-17 23:33:12 -0700

    Basic styling for comment and post body

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ profile_pictures = {
 "coral" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_fill,h_300,w_300/ttqgaopixx6aofkcvakj.jpg",
 "magpie" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_fill,h_300,w_300/kxrv2avetle0cw7wpdpr.png",
 "deerhunter9000" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_fill,h_300,w_300/z2l2nfvaowdeqvsmfyx5.png",
-"bettysings" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_fill,h_300,w_300/eqmmohyxfeduf6nsdsuw.jpg",
+"bethe" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_fill,h_300,w_300/eqmmohyxfeduf6nsdsuw.jpg",
 "carol" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_fill,h_300,w_300/enlialgapjksbqfism6k.jpg",
 "default" => "http://res.cloudinary.com/dvd7awvbl/image/upload/c_limit,h_300,w_300/v1434576931/t604klj869c13xolh9pf.png"
 }
@@ -13,17 +13,54 @@ User.create(username: "sherrif", email: 'rick.grimes@dead.org', password: 'passw
 User.create(username: "coral", email: 'carl.grimes@dead.org', password: 'password', activated: true)
 User.create(username: "michonne", email: 'michonne@dead.org', password: 'password', activated: true)
 User.create(username: "carol", email: 'carol@dead.org', password: 'password', activated: true)
-User.create(username: "bettysings", email: 'beth.green@dead.org', password: 'password', activated: true)
+User.create(username: "bethe", email: 'beth.green@dead.org', password: 'password', activated: true)
 User.create(username: "deerhunter9000", email: 'daryl.dixon@dead.org', password: 'password', activated: true)
 User.create(username: "magpie", email: 'maggie.green@dead.org', password: 'password', activated: true)
 
+User.find_by(username: "bethe").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/idkghwwqg6tsxvyihrte.jpg",
+  body: "New hair cut!")
+
+
+User.find_by(username: "carol").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/mwysqpjikstacaw7sdkw.jpg",
+  body: "Lookout duty")
+
+User.find_by(username: "deerhunter9000").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/ykotlikxpx2vsox2ai24.jpg",
+  body: "Check out ma sweet hog")
+
+User.find_by(username: "deerhunter9000").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/ant08cbigunagghqlhhg.png",
+  body: "Cleaning my crossbow")
+
+User.find_by(username: "bethe").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/mg3af4uz8owqa1rzawqo.jpg",
+  body: "Tickling the ivories!")
+
+User.find_by(username: "coral").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/nnf5v2usse7515nhlxwb.jpg",
+  body: "I found this place yesterday. Let's see if there are supplies!")
+
+User.find_by(username: "michonne").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/riwl28vpb44y4edyhplv.jpg",
+  body: "Don't even...")
+
+User.find_by(username: "sherrif").posts.create(
+  image_url: "http://res.cloudinary.com/dvd7awvbl/image/upload/c_scale,w_600/lnnrvczzmh7mdsprjkn7.jpg",
+  body: "Anyone want to play scrabble later? Coral?")
+
+
+
+
+
 
 User.all.each do |user|
-  5.times do |n|
-    user.posts.create!(body: Faker::Hacker.say_something_smart,
-                       image_url: Faker::Avatar.image(SecureRandom.urlsafe_base64(8), "500x500")
-                       )
-  end
+  # 5.times do |n|
+  #   user.posts.create!(body: Faker::Hacker.say_something_smart,
+  #                      image_url: Faker::Avatar.image(SecureRandom.urlsafe_base64(8), "500x500")
+  #                      )
+  # end
 
   pic = profile_pictures[user.username]
   thumb = pic.split("h_300,w_300").join("h_100,w_100")
